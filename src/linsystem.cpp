be48a049fc7850dedc98cbbd1d20cfaaae773651
Martin Butnikosarovski <butni@kma.zcu.cz>
2010-04-07 14:54:16 -0700

    Fixed forgotten enlarging of the arays in linsystem and feproblem. Enlarging is a consequence of adding variable order on edges.

--- src/linsystem.cpp
+++ src/linsystem.cpp
@@ -542,8 +542,8 @@ void LinSystem::assemble(bool rhsonly)
         refmap[j].force_transform(pss[j]->get_transform(), pss[j]->get_ctm());
       }
       marker = e0->marker;
-
       init_cache();
+
       //// assemble volume bilinear forms //////////////////////////////////////
       for (unsigned int ww = 0; ww < s->bfvol.size(); ww++)
       {
@@ -616,8 +616,7 @@ void LinSystem::assemble(bool rhsonly)
         }
       }
 
-
-      // assemble surface integrals now: loop through boundary edges of the element
+       // assemble surface integrals now: loop through boundary edges of the element
       for (unsigned int edge = 0; edge < e0->nvert; edge++)
       {
         if (!bnd[edge]) continue;
@@ -671,7 +670,6 @@ void LinSystem::assemble(bool rhsonly)
           if (!nat[m]) continue;
           ep[edge].base = trav.get_base();
           ep[edge].space_v = spaces[m];
-
           for (int i = 0; i < am->cnt; i++)
           {
             if (am->dof[i] < 0) continue;
@@ -679,6 +677,7 @@ void LinSystem::assemble(bool rhsonly)
             RHS[am->dof[i]] += eval_form(lfs, fv, refmap+m, ep+edge) * am->coef[i];
           }
         }
+
       }
       delete_cache();
     }
@@ -906,7 +905,6 @@ scalar LinSystem::eval_form(WeakForm::LiFormSurf *lf, PrecalcShapeset *fv, RefMa
   int eo = quad->get_edge_points(ep->edge);
   double3* pt = quad->get_points(eo);
   int np = quad->get_num_points(eo);
-
   // init geometry and jacobian*weights
   if (cache_e[eo] == NULL)
   {
@@ -918,13 +916,10 @@ scalar LinSystem::eval_form(WeakForm::LiFormSurf *lf, PrecalcShapeset *fv, RefMa
   }
   Geom<double>* e = cache_e[eo];
   double* jwt = cache_jwt[eo];
-
   // function values and values of external functions
   Func<double>* v = get_fn(fv, rv, eo);
   ExtData<scalar>* ext = init_ext_fns(lf->ext, rv, eo);
-
   scalar res = lf->fn(np, jwt, v, e, ext);
-
   ext->free(); delete ext;
   return 0.5 * res; // Edges are parametrized from 0 to 1 while integration weights
                     // are defined in (-1, 1). Thus multiplying with 0.5 to correct
